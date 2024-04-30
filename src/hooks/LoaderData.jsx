a62648d71d5ab5bb4118504e438d4f4ed60b7a7d
Fabian Mendez <ofmendez@gmail.com>
2024-04-30 11:39:04 -0500

    Update download file name in LoaderData.jsx

--- src/hooks/LoaderData.jsx
+++ src/hooks/LoaderData.jsx
@@ -82,7 +82,7 @@ const fetchAll = ({ loadingDownload, setLoadingDownload }) => {
 				const url = window.URL.createObjectURL(new Blob([blob]));
 				const link = document.createElement('a');
 				link.href = url;
-				link.setAttribute('download', 'terms.csv');
+				link.setAttribute('download', 'terms.txt');
 				document.body.appendChild(link);
 				link.click();
 				link.parentNode.removeChild(link);
