8f0e3364d211111d85c1c6830caee6e6840f1e25
Andres Garcia <andresgarcia@tretto.co>
2024-05-03 17:08:30 -0500

    Desarrollado por

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -71,6 +71,8 @@ const Menu = () => {
 					</div>
 					<div className='MenuLateralSeccion InformacionPropiedadAplicativo'>
 						© 2024 Explore Diccionario Jurídico Minero-Energético.
+						<br></br>
+						Desarrollado por <a href="https://ricaurteruedaabogados.com/" target="_blank">Ricaurte Rueda Abogados</a>.
 					</div>
 				</div>
 			</div>
