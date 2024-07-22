9fcc01460608c640ba7aec51b2f58c0db82d42df
Andres Garcia <andresgarcia@tretto.co>
2024-07-22 09:20:10 -0500

    Retirada url fuente

--- src/components/ViewMeaning.jsx
+++ src/components/ViewMeaning.jsx
@@ -39,8 +39,8 @@ const ViewMeaning = ({ meaning, query, id }) => {
 			</div>
 			<div className='SeparadorSecciones' />
 			<p className='InformacionDefinicion'>FUENTE: {getHLText(meaning.source)}</p>
-			<a className='EnlaceFuente' href={meaning.file ? meaning.file : 'https://diccionarioexplore.com/'} target='_blank' rel='noreferrer'>
-				{meaning.file ? meaning.file : 'https://diccionarioexplore.com/'}
+			<a className='EnlaceFuente' href={meaning.file ? meaning.file : ''} target='_blank' rel='noreferrer'>
+				{meaning.file ? meaning.file : ''}
 			</a>
 		</div>
 	);
