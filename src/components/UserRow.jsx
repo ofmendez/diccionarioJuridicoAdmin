d34b22700eb98aebfe7b4641c18dbce99cf77185
Fabian Mendez <ofmendez@gmail.com>
2024-10-17 19:31:09 -0500

    refactor: Update last login display in UserRow component

--- src/components/UserRow.jsx
+++ src/components/UserRow.jsx
@@ -1,13 +1,19 @@
 import { Link } from 'react-router-dom';
-
 import { IconoEditar, IconoVer } from '@components/icons.js';
 
 const UserRow = ({ user }) => {
+	const getLastLogin = () => {
+		if (user.lastLogin)
+			return new Date(user.lastLogin).toLocaleDateString('es-CO');
+		if (user.initSuscription)
+			return new Date(user.initSuscription).toLocaleDateString('es-CO');
+		return '--/--/----';
+	};
 	return (
 		<tr>
 			<td>{user.name}</td>
 			<td>{user.role}</td>
-			<td>{user.lastLogin}</td>
+			<td>{getLastLogin()}</td>
 			<td className='TablaTextoCentrado'>
 				<div className='TablaContenidoAcciones'>
 					<Link to={`/users/${user._id}`}>
