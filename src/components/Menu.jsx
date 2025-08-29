c7c659497862373233bc995b01485de165a3ebef
Andres Garcia <andresgarcia@tretto.co>
2025-08-29 15:48:39 -0500

    Instrucciones

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -69,8 +69,11 @@ const Menu = () => {
 							</li>
 						</ul>
 					</div>
+					<div class="IntruccionesAplicativo">
+						En cada uno de los términos de este diccionario Usted encontrará Primero: las normas sobre la materia, organizadas de lo más reciente a lo más antiguo; Segundo: la jurisprudencia sobre el tema de la más reciente a la más antigua; y Tercero la doctrina de las autoridades (Ministerio de Minas y Energía, Agencia Nacional de Minería, Ministerio de Ambiente y Desarrollo Sostenible) sobre la materia
+					</div>
 					<div className='MenuLateralSeccion InformacionPropiedadAplicativo'>
-						© 2024 Explore Diccionario Jurídico Minero-Energético.
+						© 2025 Explore Diccionario Jurídico Minero-Energético.
 						<br />
 						Desarrollado por <a href='https://ricaurteruedaabogados.com/' target='_blank' rel='noreferrer'>Ricaurte Rueda Abogados</a>.
 					</div>
